95fd3ff811f5df81ef927f30c7cf3a3c195e2be7
nicoledodge <nicoledodge5@gmail.com>
2023-05-16 01:10:59 -0500

    sign up page and configs

--- sites/gh-pages/src/App.tsx
+++ sites/gh-pages/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import classNames from "classnames";
 import Datetime from 'react-datetime';
-import {DropVariables, iUsers, postUser} from '@drop-in-gaming/core';
+import {DropVariables, getStyles, Input, iUsers, postUser} from '@drop-in-gaming/core';
 import {useState} from "react";
 import moment from "moment/moment";
 
 
-function App() {
+export default function App() {
 
   let [userSignUpAndInInformation, setUserSignUpAndInInformation] = useState<iUsers>({})
 
@@ -17,147 +18,143 @@ function App() {
 
   const unregisteredInvite = new URLSearchParams(window.location.search).get('referenceId');
 
+  const dig = getStyles()
 
-  return (
+
+    return (
       <div className="App">
         <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo"/>*/}
-          {/*<p>*/}
-          {/*    Edit <code>src/App.js</code> and save to reload.*/}
-          {/*</p>*/}
-          {/*<a*/}
-          {/*    className="App-link"*/}
-          {/*    href="https://reactjs.org"*/}
-          {/*    target="_blank"*/}
-          {/*    rel="noopener noreferrer"*/}
-          {/*>*/}
-          {/*    Learn React*/}
-          {/*</a>*/}
-          <div id="login-form" role="tabpanel"
-               aria-labelledby="login-tab">
-            <div
-                role="form">
-              <div>
-                <div>
-                  <div
-                      id="register-form" role="tabpanel"
-                      aria-labelledby="register-tab">
-                    <div role="form">
+            <div className={classNames(
+                dig.tabPane, dig.show, dig.active
+            )} id="login-form" role="tabpanel"
+                 aria-labelledby="login-tab">
+                <div className={classNames(
+                    dig.bgTransparent, dig.rounded0, dig.border0,
+                )}
+                     role="form">
+                    <div className={classNames(
+                        dig.card, dig.rounded0, dig.border0, dig.bgTransparent
+                    )}>
+                        <div className={classNames(
+                            dig.cardBody, dig.digBgNeutral2
+                        )}>
+                            <div className={classNames(dig.tabPane)} id="register-form" role="tabpanel"
+                                 aria-labelledby="register-tab">
+                                <div className={classNames(dig.bgTransparent, dig.rounded0, dig.border0)}
+                                     role="form">
 
 
-                      <label>UserName</label>
-                      <input
-                          required
-                          id="dig-register-username" name="user_login" type="text"
-                          placeholder="Enter Username" defaultValue=""
-                          onChange={(e) => {
-                            setUserSignUpAndInInformation({
-                              ...userSignUpAndInInformation,
-                              user_login: e.target.value
-                            })
-                          }}/>
+                                    <div
+                                        className={classNames(dig.card, dig.rounded0, dig.border0, dig.bgTransparent)}>
+                                        <div className={classNames(dig.cardBody, dig.borderColorNeutral2)}>
 
-                      <label>{
-                        <>E-mail Address
-                          <i id="dig_question_mark_popup"
-                             onMouseEnter={changeEmailWarningOpened}
-                             onMouseLeave={changeEmailWarningOpened}/>
-                          {emailWarningOpened && <>
-                            <div>
-                              We will not share your email address with anyone.
+                                            <Input
+                                                label={'Username'}
+                                                required
+                                                className={classNames(dig.formControl, dig.border0, dig.rounded0, dig.borderColorNeutral3,
+                                                    dig.fontSize1P2Em)}
+                                                id="dig-register-username" name="user_login" type="text"
+                                                placeholder="Enter Username" defaultValue=""
+                                                onChange={(e) => {
+                                                    setUserSignUpAndInInformation({
+                                                        ...userSignUpAndInInformation,
+                                                        user_login: e.target.value
+                                                    })
+                                                }}/>
+
+
+                                            <Input
+                                                label={
+                                                    <>E-mail Address
+                                                        <i className={classNames("fas fa-question-circle")}
+                                                           id="dig_question_mark_popup"
+                                                           onMouseEnter={changeEmailWarningOpened}
+                                                           onMouseLeave={changeEmailWarningOpened}/>
+                                                        {emailWarningOpened && <>
+                                                            <div
+                                                                className={classNames("dig_question_mark_popup_text")}>
+                                                                We will not share your email address with anyone.
+                                                            </div>
+                                                        </>}
+                                                    </>}
+                                                className={classNames(dig.formControl, dig.border0, dig.rounded0, dig.borderColorNeutral3,
+                                                    dig.fontSize1P2Em)}
+                                                id="dig-register-user-email" name="user_email" type="email"
+                                                placeholder="Enter your E-mail" defaultValue=""
+                                                onChange={(e) => {
+                                                    setUserSignUpAndInInformation({
+                                                        ...userSignUpAndInInformation,
+                                                        user_email: e.target.value
+                                                    })
+                                                }}/>
+
+
+                                            <Input
+                                                label={'Password'}
+                                                className={classNames(dig.formControl, dig.border0, dig.rounded0, dig.borderColorNeutral3,
+                                                    dig.fontSize1P2Em)}
+                                                id="new_password" name="user_password" type="password"
+                                                defaultValue=""
+                                                placeholder="Enter Password"
+                                                onChange={(e) => {
+                                                    setUserSignUpAndInInformation({
+                                                        ...userSignUpAndInInformation,
+                                                        user_pass: e.target.value
+                                                    })
+                                                }}/>
+
+
+                                            Birthday
+                                            <div onClick={() => setToday(today)}
+                                                 className={classNames(dig.formControl, dig.border0, dig.rounded0, dig.borderColorNeutral3,
+                                                     dig.fontSize1P2Em)}
+                                                 style={{color: 'black'}}>
+                                                <Datetime
+                                                    dateFormat={DropVariables.momentGeneralDateFormat}
+                                                    initialValue={today}
+                                                    timeConstraints={{
+                                                        hours: {min: 0, max: 72, step: 1}
+                                                    }}
+                                                />
+                                            </div>
+
+
+
+                                        </div>
+                                    </div>
+
+                                    <div
+                                        className={classNames(dig.rounded0, dig.dFlex, dig.justifyContentCenter, dig.border, dig.mt3)}>
+                                        <button
+                                            className={classNames(dig.btn, dig.btnPrimary, dig.btnLg, dig.rounded0, dig.fwBold, dig.w100, dig.textUppercase, "dig-btn-green pull-right dig-register font-size-1em")}
+                                            type="submit"
+                                            onClick={() => {
+
+                                                console.log('join')
+
+                                                postUser({
+                                                    user_pass: userSignUpAndInInformation.user_pass,
+                                                    user_login: userSignUpAndInInformation.user_login,
+                                                    user_email: userSignUpAndInInformation.user_email,
+                                                    temp_invite_id: unregisteredInvite ? parseInt(unregisteredInvite) : undefined,
+                                                    success: () => {
+                                                        return 'Account created!'
+                                                    }
+                                                })
+
+
+                                            }}>
+                                            Join
+                                        </button>
+                                    </div>
+
+                                </div>
                             </div>
-                          </>}
-                        </>}</label>
-
-                      <input
-                          id="dig-register-user-email" name="user_email" type="email"
-                          placeholder="Enter your E-mail" defaultValue=""
-                          onChange={(e) => {
-                            setUserSignUpAndInInformation({
-                              ...userSignUpAndInInformation,
-                              user_email: e.target.value
-                            })
-                          }}/>
-
-                      <label>Password</label>
-                      <input
-                          id="new_password" name="user_password" type="password"
-                          defaultValue=""
-                          placeholder="Enter Password"
-                          onChange={(e) => {
-                            setUserSignUpAndInInformation({
-                              ...userSignUpAndInInformation,
-                              user_pass: e.target.value
-                            })
-                          }}/>
-
-
-                      Birthday
-                      <div onClick={() => setToday(today)}
-                           style={{color: 'black'}}>
-                        <Datetime
-                            dateFormat={DropVariables.momentGeneralDateFormat}
-                            initialValue={today}
-                            timeConstraints={{
-                              hours: {min: 0, max: 72, step: 1}
-                            }}
-                        />
-                      </div>
-
-                      {/*<label>How Did You Hear About Us?</label>*/}
-                      {/*<select  name="referral-source"*/}
-                      {/*        id="referral-source"*/}
-                      {/*        options={[*/}
-                      {/*            {value: 'None'},*/}
-                      {/*            {value: 'Twitch'},*/}
-                      {/*            {value: 'Twitter'},*/}
-                      {/*            {value: 'Instagram'},*/}
-                      {/*            {value: 'FaceBook'},*/}
-                      {/*            {value: 'YouTube'},*/}
-                      {/*            {value: 'Referral'},*/}
-                      {/*            {value: 'Other'},*/}
-                      {/*        ]}*/}
-                      {/*        aria-hidden="true">*/}
-                      {/*</select>*/}
-
-                      {/*<input label={'Referral Code'}*/}
-                      {/*       defaultValue={''}*/}
-                      {/*       type="text"/>*/}
-
-                      {/*<input label={'Source'}*/}
-                      {/*       type="text"*/}
-                      {/*       defaultValue={""}/>*/}
-
-
+                        </div>
                     </div>
-                  </div>
-
-                  <div>
-                    <button
-                        type="submit"
-                        onClick={() => {
-
-                          console.log('join')
-
-                          postUser({
-                            user_pass: userSignUpAndInInformation.user_pass,
-                            user_login: userSignUpAndInInformation.user_login,
-                            user_email: userSignUpAndInInformation.user_email,
-                            temp_invite_id: unregisteredInvite ? parseInt(unregisteredInvite) : undefined,
-                            success: () => {
-                              return 'Account created!'
-                            }
-                          })
-
-
-                        }}>
-                      Join
-                    </button>
-                  </div>
-
                 </div>
-              </div>
-            </div>
+
+
 
           </div>
         </header>
@@ -165,4 +162,3 @@ function App() {
   );
 }
 
-export default App;
